a42a9390bfd2d66d47291622146abdcd57ff034e
Manoj Kumar <pk9017777273@gmail.com>
2026-09-13 14:35:15 +0530

    style(oneks): align day2 profile regression

--- src/oneks/test/day2_lifecycle_test.rb
+++ src/oneks/test/day2_lifecycle_test.rb
@@ -62,7 +62,7 @@ class Day2LifecycleTest < Minitest::Test
         flavour = conf.fetch('flavours').fetch('standalone')
         assert_equal true, flavour.fetch('override_defaults')
         assert_equal 1, flavour.fetch('defaults').fetch('count')
-        count = conf.fetch('user_inputs').find { |input| input.fetch('name') == 'count' }
+        count = conf.fetch('user_inputs').find {|input| input.fetch('name') == 'count' }
         assert_equal 1, count.dig('match', 'values', 'min')
         assert_equal 7, count.dig('match', 'values', 'max')
 
