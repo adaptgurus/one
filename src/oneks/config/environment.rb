cd2064106700351d4c2588c123546f4ca7c0edc0
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-09-13 07:17:07 +0530

    feat(oneks): load day-2 lifecycle services

--- src/oneks/config/environment.rb
+++ src/oneks/config/environment.rb
@@ -34,6 +34,7 @@ require 'uri'
 require_relative File.join(APP_ROOT, 'lib', 'helpers', 'appliance_helper')
 require_relative File.join(APP_ROOT, 'lib', 'helpers', 'oneks_helper')
 require_relative File.join(APP_ROOT, 'lib', 'helpers', 'k8s_helper')
+require_relative File.join(APP_ROOT, 'lib', 'helpers', 'k8s_day2_helper')
 require_relative File.join(APP_ROOT, 'lib', 'fakes', 'k8s_fake')
 
 OneHelper::Image.extend(OneKS::OneHelperExtensions::Image)
@@ -44,6 +45,7 @@ require_relative File.join(APP_ROOT, 'app', 'models', 'deployment')
 require_relative File.join(APP_ROOT, 'app', 'models', 'k8s_dependency')
 require_relative File.join(APP_ROOT, 'app', 'models', 'k8s_group')
 require_relative File.join(APP_ROOT, 'app', 'models', 'dependencies', 'seed_vm')
+require_relative File.join(APP_ROOT, 'app', 'models', 'dependencies', 'seed_vm_heartbeat')
 require_relative File.join(APP_ROOT, 'app', 'models', 'dependencies', 'cluster_router')
 require_relative File.join(APP_ROOT, 'app', 'models', 'groups', 'controlplane')
 require_relative File.join(APP_ROOT, 'app', 'models', 'groups', 'nodegroup')
@@ -55,6 +57,7 @@ require_relative File.join(APP_ROOT, 'app', 'services', 'cluster_readiness')
 require_relative File.join(APP_ROOT, 'app', 'services', 'cluster_lcm')
 require_relative File.join(APP_ROOT, 'app', 'services', 'event_manager', 'event_manager')
 require_relative File.join(APP_ROOT, 'app', 'services', 'cluster_wd')
+require_relative File.join(APP_ROOT, 'app', 'services', 'lifecycle_status')
 
 # Controllers
 APP_PATHS = [File.join(APP_ROOT, 'app', 'controllers')]
