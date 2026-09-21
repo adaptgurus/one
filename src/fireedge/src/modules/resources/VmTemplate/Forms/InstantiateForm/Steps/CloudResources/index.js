00c29d82ff4806b5674893c96eba4e573df2ba41
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-09-21 10:39:37 +0000

    fix(layersentry): subscribe VM resource step to visible networks

--- src/fireedge/src/modules/resources/VmTemplate/Forms/InstantiateForm/Steps/CloudResources/index.js
+++ src/fireedge/src/modules/resources/VmTemplate/Forms/InstantiateForm/Steps/CloudResources/index.js
@@ -17,11 +17,15 @@ import { Alert, Stack, Typography } from '@mui/material'
 import PropTypes from 'prop-types'
 
 import { FormWithSchema } from '@ComponentsModule'
+import { VnAPI } from '@FeaturesModule'
 import { SCHEMA, SECTIONS } from './schema'
 
 export const STEP_ID = 'resources'
 
-const Content = ({ vmTemplate }) => (
+const Content = ({ vmTemplate }) => {
+  const { data: networks = [] } = VnAPI.useGetVNetworksQuery()
+
+  return (
   <Stack gap={3} sx={{ maxWidth: 1040, width: '100%', mx: 'auto' }}>
     <div>
       <Typography variant="h5" fontWeight={700}>
@@ -38,7 +42,7 @@ const Content = ({ vmTemplate }) => (
       automatically. Add only the extra data disk you need.
     </Alert>
 
-    {SECTIONS(vmTemplate).map(({ id, legend, fields }) => (
+    {SECTIONS(vmTemplate, networks).map(({ id, legend, fields }) => (
       <FormWithSchema
         key={id}
         id={STEP_ID}
@@ -56,7 +60,8 @@ const Content = ({ vmTemplate }) => (
       you explicitly enable a speed limit.
     </Alert>
   </Stack>
-)
+  )
+}
 
 Content.propTypes = {
   vmTemplate: PropTypes.object,
