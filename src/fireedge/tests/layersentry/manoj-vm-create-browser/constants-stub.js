3d0e002a793bd14ace5171eedcf1a855eef58bef
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-09-23 21:28:58 +0000

    qa: add Manoj VM create browser audit constants-stub.js

---
+++ src/fireedge/tests/layersentry/manoj-vm-create-browser/constants-stub.js
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+export const DEFAULT_TEMPLATE_LOGO = 'default.png'
+export const STATIC_FILES_URL = '/static'
+export const TEXT_VARIANTS = { H6: 'h6' }
+export const TEXT_WEIGHTS = { SEMIBOLD: 600 }
+export const UNITS = { MB: 'MB' }
+export const T = {
+  Name: 'Name',
+  CPU: 'CPU',
+  Memory: 'Memory',
+  Architecture: 'Architecture',
+  State: 'State',
+  Close: 'Close',
+}
+export const PATH = {
+  INSTANCE: {
+    VMS: {
+      LIST: '/vm',
+      DETAIL: '/vm/:id',
+    },
+  },
+  TEMPLATE: {
+    VMS: {
+      INSTANTIATE: '/vm-template/instantiate',
+    },
+  },
+}
